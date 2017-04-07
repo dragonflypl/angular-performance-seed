c63e449ac99470837f0bce56d47ac69f1eabf34d
waszczpa <pawel.waszczynski@ubs.com>
2017-04-07 11:35:00 +0200

    30-applyasync

---
+++ app/view1/balance-animation.directive.js
@@ -0,0 +1,50 @@
+(function() {
+
+	angular.module('myApp.view1').directive('balanceAnimation', function($compile) {
+
+		function animate(element, scope) {
+			scope.$applyAsync(function() {
+                if (elementInViewport(element)) {
+                    scope.$applyAsync(function() {
+                        element.classList.add('balance-animation');
+                    });
+                } else {
+                    scope.$applyAsync(function() {
+                        element.classList.remove('balance-animation');
+                    });
+                }
+			})
+		}
+
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs, controller) {
+				animate(element[0], scope);
+				window.addEventListener('scroll', function() {
+					animate(element[0], scope);
+				});
+			}
+		}
+	});
+
+	function elementInViewport(el) {
+	  var top = el.offsetTop;
+	  var left = el.offsetLeft;
+	  var width = el.offsetWidth;
+	  var height = el.offsetHeight;
+
+	  while(el.offsetParent) {
+	    el = el.offsetParent;
+	    top += el.offsetTop;
+	    left += el.offsetLeft;
+	  }
+
+	  return (
+	    top >= window.pageYOffset &&
+	    left >= window.pageXOffset &&
+	    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+	    (left + width) <= (window.pageXOffset + window.innerWidth)
+	  );
+	}
+
+}())
