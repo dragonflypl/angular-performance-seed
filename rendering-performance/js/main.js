7c553e018d41ae0ce8d2aa103eb047a6b62c0706
Paweł Waszczyński <waszczynski@gmail.com>
2017-04-12 19:37:13 +0200

    Rendering performance

---
+++ rendering-performance/js/main.js
@@ -0,0 +1,10 @@
+window.measureCRP = function() {
+    var t = window.performance.timing,
+        interactive = t.domInteractive - t.domLoading,
+        dcl = t.domContentLoadedEventStart - t.domLoading,
+        complete = t.domComplete - t.domLoading;
+    var stats = document.createElement('p');
+    stats.textContent = 'interactive: ' + interactive + 'ms, ' +
+        'dcl: ' + dcl + 'ms, complete: ' + complete + 'ms';
+    document.body.appendChild(stats);
+}
